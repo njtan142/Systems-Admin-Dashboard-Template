a395e8010cddd7825730a15e6c84dbb8286e4d84
Niño James A. Tan <njtan142@gmail.com>
2024-08-04 15:58:27 +0800

    template

---
+++ src/apis/firebase/firebase.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+/**
+ * @todo: Replace the firebaseConfig with the actual config
+ */
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+const getTemporaryAuth = () => {
+  const tempApp = initializeApp(firebaseConfig, "temp");
+  return getAuth(tempApp);
+}
+
+export { firestore, auth, getTemporaryAuth, storage };
